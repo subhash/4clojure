13a11cadc1d3ce5f9b9973c9f4d8f84351975c48
Subhash Gopalakrishnan <subhashgo@gmail.com>
2014-12-30 22:22:52 +0530

    Not lazy yet

---
+++ 82-word-chains.clj
@@ -0,0 +1,30 @@
+(defn chain [words]
+  (letfn [(pad [w n]
+            (if-not (> n (count w))
+              [w]
+              (map #(->> w
+                         (split-at %)
+                         (interpose [nil])
+                         (apply concat))
+                   (range (inc (count w))))))
+          (diff [a b] (->> (map not= a b) (filter identity) (count)))
+          (compat? [a b]
+            (let [all (sort-by count [a b])
+                  mn (first all)
+                  mx (last all)]
+              (if (> (- (count mx) (count mn)) 1) 
+                false
+                (some #(<= (diff mx %) 1) (pad mn (count mx))))
+              ))
+          (compat-chain? [c]
+            (every? #(compat? (first %) (last %)) (partition 2 1 c)))
+          (perm [c]
+            (if-not (seq c) [[]]
+                    (for [x c
+                          y (perm (for [z c :when (not= z x)] z))]
+                      (conj y x))))
+          ]
+    (let [chains (perm (vec words))]
+      (not= nil (some compat-chain? chains) )
+      )))
+
