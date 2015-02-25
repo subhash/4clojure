ee25d7eaf3f41e65c797f839a0597dd1f390d10b
Subhash Gopalakrishnan <subhashgo@gmail.com>
2015-02-25 21:16:51 +0530

    Update 150-palindrome-numbers.clj

--- 150-palindrome-numbers.clj
+++ 150-palindrome-numbers.clj
@@ -1,15 +1,24 @@
-(defn pal [n]
-  (letfn [(itoa [n] (map read-string (map str (str n))))
-          (atoi [s] (read-string (apply str s)))
-          (next-pal [n]
-            (let [s (itoa n)
-                  cnt (count s)
-                  parts [(take (quot (inc cnt) 2) s) (reverse (drop (quot cnt 2) s))] 
-                  [l r] (map atoi parts)
-                  f (itoa (if (> l r) l (inc l)))
-                  fs (concat f (reverse (if (> (* 2 (count f)) cnt) (butlast f) f)))]
-              (atoi fs)))]
-    (let [res (iterate next-pal n)
-          fres (->> (first res) itoa)]
-      (if (= fres (reverse fres)) res (rest res)))))
-
+(fn pal [n]
+(letfn [
+  (itoa [n] (->> n str (map str) (map read-string)))
+  (atoi [a] (->> a (apply str) read-string))
+  (pal? [n] (= (itoa n) (reverse (itoa n))))
+  (split [c] (let [m (/ (count c) 2)]
+               [(take m c) (drop m c)]))
+  (new-pair [l r] (let [nl (-> l atoi inc itoa)
+                        nr (drop (- (count nl) (count r)) (reverse nl))]
+                    [nl nr]))
+  (nextpal [n]
+  (let [c (itoa n)
+        [l r] (split c)
+        [nl nr] (new-pair l r)
+        nc (concat nl nr)]
+    (if (= n 9) 11 (atoi nc))))
+  (pal-or-next [n]
+    (if (pal? n) n
+      (let [c (itoa n)
+            [l r] (split c)
+            nc (concat l (drop (- (count l) (count r)) (reverse l)))
+            nn (atoi nc)]
+        (if (> nn n) nn (nextpal n)))))]
+  (iterate nextpal (pal-or-next n))))
