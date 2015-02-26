f7ff18efeb818e804c0fd4a534e58cf04c88e339
Subhash Gopalakrishnan <subhashgo@gmail.com>
2015-02-26 21:58:54 +0530

    Inspired by chouser's solution

--- 150-palindrome-numbers.clj
+++ 150-palindrome-numbers.clj
@@ -1,24 +1,22 @@
-(fn pal [n]
-(letfn [
-  (itoa [n] (->> n str (map str) (map read-string)))
-  (atoi [a] (->> a (apply str) read-string))
-  (pal? [n] (= (itoa n) (reverse (itoa n))))
-  (split [c] (let [m (/ (count c) 2)]
-               [(take m c) (drop m c)]))
-  (new-pair [l r] (let [nl (-> l atoi inc itoa)
-                        nr (drop (- (count nl) (count r)) (reverse nl))]
-                    [nl nr]))
-  (nextpal [n]
-  (let [c (itoa n)
-        [l r] (split c)
-        [nl nr] (new-pair l r)
-        nc (concat nl nr)]
-    (if (= n 9) 11 (atoi nc))))
-  (pal-or-next [n]
-    (if (pal? n) n
-      (let [c (itoa n)
-            [l r] (split c)
-            nc (concat l (drop (- (count l) (count r)) (reverse l)))
-            nn (atoi nc)]
-        (if (> nn n) nn (nextpal n)))))]
-  (iterate nextpal (pal-or-next n))))
+(defn pal [n]
+  (letfn [
+
+( atoi [c] (Long. (apply str c)))
+( countn [n] (count (str n)))
+( firstn [n] (atoi (take (/ (countn n) 2) (str n))))
+( fold1 [n] (atoi (concat (str n) (reverse (str n)))))
+( fold2 [n] (atoi (concat (str n) (rest (reverse (str n))))))
+( fold3 [n] (atoi (concat (str n) (butlast (reverse (str n))))))
+( makepal [n] 
+  ((if (even? (countn n)) fold1 fold2) (firstn n)))
+( nextpal [n] 
+  (let [nxt (inc (firstn n))
+        folded 
+        (if (even? (countn n)) 
+         (fold1 nxt)
+         (fold2 nxt))]
+    (if (> (countn nxt) (countn (firstn n))) 
+      (makepal (quot folded 10)) 
+       folded)))]  
+    (filter (partial <= n) (iterate nextpal (makepal n)))))
+
